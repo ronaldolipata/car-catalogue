fd54bedfbe24044abb3a67060608ea82aec8b58d
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2024-01-26 20:49:40 +0800

    refac: set the timestamps to true

--- backend/models/Car.js
+++ backend/models/Car.js
@@ -2,26 +2,29 @@ import mongoose from 'mongoose';
 
 const { Schema, model } = mongoose;
 
-const carSchema = new Schema({
-  carModel: {
-    brand: {
-      type: String,
-      require: true,
-    },
-    model: {
-      type: String,
-      require: true,
-    },
-    year: {
-      type: String,
-      require: true,
-    },
-    color: {
-      type: String,
-      require: true,
+const carSchema = new Schema(
+  {
+    carModel: {
+      brand: {
+        type: String,
+        require: true,
+      },
+      model: {
+        type: String,
+        require: true,
+      },
+      year: {
+        type: String,
+        require: true,
+      },
+      color: {
+        type: String,
+        require: true,
+      },
     },
   },
-});
+  { timestamps: true }
+);
 
 const Car = model('Car', carSchema);
 export default Car;
